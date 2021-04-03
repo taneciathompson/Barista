a20c8ea4c2f40c9d8fffa8db21f2b83298bdcb52
Tanecia Thompson <taneciathompson@gmail.com>
2021-04-03 11:48:49 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+    'url' : "mongodb+srv://mari27:code27@cluster0.3g7gm.mongodb.net/barista?retryWrites=true&w=majority",
+    'dbName': 'barista'
+};
+
+/*
+heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
+*/
